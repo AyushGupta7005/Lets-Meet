ac46314b80205dfc231d50363148c0a6f234af74
AyushGupta7005 <12321ayushgupta@gmail.com>
2025-08-05 19:22:58 +0530

    add environment variables for GitHub and Google authentication

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,10 +2,28 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db"; // your drizzle instance
 import * as schemas from "@/db/schema"; // your schemas
+import {
+  githubClientId,
+  githubClientSecret,
+  googleClientId,
+  googleClientSecret,
+} from "../../env.config";
 
 export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
+  },
+  socialProviders: {
+    github: {
+      enabled: true,
+      clientId: githubClientId,
+      clientSecret: githubClientSecret,
+    },
+    google: {
+      enabled: true,
+      clientId: googleClientId,
+      clientSecret: googleClientSecret,
+    },
   },
   database: drizzleAdapter(db, {
     schema: {
