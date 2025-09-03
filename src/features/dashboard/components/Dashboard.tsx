069ca3fc88af43b789d23e9a170ec0c9ad0d1cae
AyushGupta7005 <12321ayushgupta@gmail.com>
2025-09-03 23:35:00 +0530

    refactor: simplify Dashboard component by removing unused imports and sign-out button

--- src/features/dashboard/components/Dashboard.tsx
+++ src/features/dashboard/components/Dashboard.tsx
@@ -1,38 +1,17 @@
 "use client";
-import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
 
 export default function Dashboard() {
-  const router = useRouter();
   const { data: session } = authClient.useSession();
+
   if (!session) {
     return <p>Loading...</p>;
   }
-
   return (
     <div className="flex flex-col items-center gap-8 p-10">
       <h1 className="mx-auto text-center text-2xl font-bold">
-        Welcome back, {session.user.name || "User"}!
+        {session.user?.name}
       </h1>
-      <Button
-        variant="outline"
-        className="mt-4"
-        onClick={() =>
-          authClient.signOut({
-            fetchOptions: {
-              onSuccess: () => {
-                router.push("/login");
-              },
-              onError: (error) => {
-                console.error("Sign out error:", error);
-              },
-            },
-          })
-        }
-      >
-        Sign Out
-      </Button>
     </div>
   );
 }
