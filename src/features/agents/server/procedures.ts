5de3aa578035262a84728685bb9ccb32cace8b30
AyushGupta7005 <12321ayushgupta@gmail.com>
2025-09-07 12:05:35 +0530

    feat: implement agent creation procedure with validation schema and protected access

--- src/features/agents/server/procedures.ts
+++ src/features/agents/server/procedures.ts
@@ -1,10 +1,38 @@
 import { db } from "@/db";
 import { agent } from "@/db/schema";
-import { createTRPCRouter, baseProcedure } from "@/trpc/init";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { agentCreateSchema } from "../schemas";
+import z from "zod";
+import { eq } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
-  getMany: baseProcedure.query(async () => {
-    const data = await db.select().from(agent);
-    return data;
-  }),
+  getOne: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input }) => {
+      const [data] = await db
+        .select()
+        .from(agent)
+        .where(eq(agent.id, input.id));
+      return data;
+    }),
+
+  getMany: protectedProcedure
+    .input(z.object({ userId: z.string() }))
+    .query(async ({ input }) => {
+      const data = await db
+        .select()
+        .from(agent)
+        .where(eq(agent.userId, input.userId));
+      return data;
+    }),
+
+  create: protectedProcedure
+    .input(agentCreateSchema)
+    .mutation(async ({ ctx, input }) => {
+      const [newAgent] = await db
+        .insert(agent)
+        .values({ ...input, userId: ctx.auth.user.id })
+        .returning();
+      return newAgent;
+    }),
 });
