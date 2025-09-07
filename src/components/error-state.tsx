2b3b80318496a6d2ba9606366f2b79b0847262c7
AyushGupta7005 <12321ayushgupta@gmail.com>
2025-09-07 19:20:30 +0530

    fix: correct class name typo in ErrorState and LoadingState components

--- src/components/error-state.tsx
+++ src/components/error-state.tsx
@@ -10,7 +10,7 @@ export default function ErrorState({
 }: LoadingStateProps) {
   return (
     <div className="flex flex-1 items-center justify-center px-8 py-4">
-      <div className="gapy6 bg-background flex flex-col items-center justify-center rounded-lg p-10 shadow-sm">
+      <div className="bg-background flex flex-col items-center justify-center gap-y-6 rounded-lg p-10 shadow-sm">
         <AlertCircleIcon className="size-6 text-red-500" />
         <div className="flex flex-col gap-y-2 text-center">
           <h6 className="text-lg font-medium">{title}</h6>
