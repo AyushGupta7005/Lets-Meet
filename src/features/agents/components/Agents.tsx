c404e4062df286b93a2e1ba2532d9a1d195f7e21
AyushGupta7005 <12321ayushgupta@gmail.com>
2025-09-07 19:20:52 +0530

    feat: implement Agents component with EmptyState and DataTable for agent management

--- src/features/agents/components/Agents.tsx
+++ src/features/agents/components/Agents.tsx
@@ -3,11 +3,28 @@ import ErrorState from "@/components/error-state";
 import LoadingState from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { DataTable } from "./data-table";
+import { columns } from "./columns";
+import EmptyState from "@/components/empty-state";
 
 export function Agents() {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+
+  return (
+    <div className="flex flex-1 flex-col gap-y-4 px-4 pb-8 md:px-8">
+      {data?.length === 0 ? (
+        <div className="my-auto">
+          <EmptyState
+            title="Create your first Agent"
+            description="Create an agent to join meetings"
+          />
+        </div>
+      ) : (
+        <DataTable data={data} columns={columns} />
+      )}
+    </div>
+  );
 }
 
 export function AgentsLoader() {
---
+++ src/features/agents/components/columns.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { TAgentGetOne } from "../types";
+import GeneratedAvatar from "@/components/generated-avatar";
+import { CornerDownRightIcon, VideoIcon } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+
+export const columns: ColumnDef<TAgentGetOne>[] = [
+  {
+    accessorKey: "name",
+    header: "Agent Name",
+    cell: ({ row }) => (
+      <div className="flex flex-col gap-y-1">
+        <div className="flex items-center gap-x-2">
+          <GeneratedAvatar
+            seed={row.original.name}
+            variant="botttsNeutral"
+            className="size-6"
+          />
+          <span className="fontsemibold capitalize">{row.original.name}</span>
+        </div>
+        <div className="ml-2 flex items-center gap-x-2">
+          <CornerDownRightIcon className="text-muted-foreground size-3" />
+          <span className="text-muted-foreground max-w-[200px] truncate text-sm capitalize">
+            {row.original.instructions}
+          </span>
+        </div>
+      </div>
+    ),
+  },
+  {
+    accessorKey: "meetingCount",
+    header: "Meetings",
+    cell: ({ row }) => (
+      <Badge variant={"outline"} className="flex items-center gap-x-2">
+        <VideoIcon className="size-4 text-blue-700" />
+        {row.original.meetingCount ?? 0}{" "}
+        {row.original.meetingCount === 0
+          ? "No Meeting"
+          : row.original.meetingCount > 1
+            ? "meetings"
+            : "meeting"}
+      </Badge>
+    ),
+  },
+];
